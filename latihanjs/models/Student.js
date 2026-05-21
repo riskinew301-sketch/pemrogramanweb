94296703af85baaf5b822fc07f7f26fc2fdd04af
riskinew301-sketch <riskinew301@gmail.com>
2026-05-21 11:47:36 +0700

    Create Student.js

---
+++ latihanjs/models/Student.js
@@ -0,0 +1,11 @@
+import Person from './Person.js';
+export default class Student extends Person {
+    constructor(name, age, nim, major){
+        super(name,age);
+        this.nim=nim;
+        this.major=major;
+    }
+    study(){
+        console.log('${this.name} NIM: ${this.nim}, Major: ${this.major}');
+    }
+}
