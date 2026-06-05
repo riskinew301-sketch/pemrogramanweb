0a1795b842d5fd49d8f284d91284ee488196f2f7
riskinew301-sketch <riskinew301@gmail.com>
2026-06-05 20:29:18 +0700

    Create helloworld.php

---
+++ latihanphp/helloworld.php
@@ -0,0 +1,22 @@
+<body>
+     <nav class="bg-white shadow-md mb-6">
+        <div class="max-w-4xl mx-auto px-4">
+            <div class="flex justify-between items-center h-16">
+                <div class="flex-shrink-0 font-bold text-xl text-blue-600">
+                    Produk
+                </div>
+                <div class="flex space-x-6 text-gray-600 font-medium">
+                    <a href="helloworld.php" class="hover:text-blue-600 transition">Utama</a>
+                    <a href="datetime.php" class="hover:text-blue-600 transition">Waktu</a>
+                    <a href="dataarray.php" class="hover:text-blue-600 transition">Produk</a>
+                    <a href="katalog.php" class="hover:text-blue-600 transition">Katalog</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <h1>
+        <?php
+        echo "Hello, World!";
+        ?>
+    </h1>
+</body>    
